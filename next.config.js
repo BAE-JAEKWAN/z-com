92f39dd064fb05cebbb3b04ca6a992880e250ef1
Bae JaeKwan <toxictears84@gmail.com>
2024-01-17 02:55:17 +0900

    github io 배포 테스트

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
+
+const debug = process.env.NODE_ENV !== "production";
+const repository = "z-com";
+
 const nextConfig = {
-  basePath: "https://bae-jaekwan.github.io/z-com",
+  reactStrictMode: true,
+  assetPrefix: !debug ? `/${repository}/` : "", // production 일때 prefix 경로
+  trailingSlash: true, // 빌드 시 폴더 구조 그대로 생성하도록
 };
 
 module.exports = nextConfig;
