a086717d5ffe2a0dd98be599d17dacd90436a70b
neo.quan <neo.quan@dktechin.com>
2024-01-28 00:00:26 +0900

    불필요한 임포트 삭제

--- src/app/(beforeLogin)/login/page.tsx
+++ src/app/(beforeLogin)/login/page.tsx
@@ -1,10 +1,6 @@
 'use client'
 
 import { redirect, useRouter } from 'next/navigation'
-import styles from '@/app/(beforeLogin)/_component/main.module.css'
-import Image from 'next/image'
-import Link from 'next/link'
-import zLogo from '/public/zlogo.png'
 import Main from '../_component/Main'
 import { useEffect } from 'react'
 
--- src/app/(beforeLogin)/page.tsx
+++ src/app/(beforeLogin)/page.tsx
@@ -1,7 +1,3 @@
-import styles from '@/app/(beforeLogin)/_component/main.module.css'
-import Image from 'next/image'
-import Link from 'next/link'
-import zLogo from '../../../public/zlogo.png'
 import Main from './_component/Main'
 
 const Home = () => {
